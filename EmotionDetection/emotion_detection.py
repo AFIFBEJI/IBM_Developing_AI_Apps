e9edcc559dd936e7d9b71047399cbd8faf4b3326
Afif Beji <afif.beji@esprit.tn>
2026-04-22 14:42:30 -0400

    Creating and packaging EmotionDetection

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    # URL of the emotion detection analysis service
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    
+    # Custom header specifying the model ID for the emotion detection analysis service
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    
+    # Constructing the request payload in the expected format
+    raw_text = { "raw_document": { "text": text_to_analyze } }
+    
+    # Sending a POST request to the emotion detection API
+    response = requests.post(
+        url,
+        headers = headers,
+        json = raw_text
+        )
+    
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+
+    # Extracting emotions and their corresponding scores from the response
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    
+    return {
+        **emotions,
+        'dominant_emotion': max(emotions, key=emotions.get)
+    }
